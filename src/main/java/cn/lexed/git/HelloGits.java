f16b06f26780c9005f35622aa88eb7213e8c38b3
123qwe <2456451789@qq.com>
2021-08-25 10:23:48 +0800

    git初步上传本地库

---
+++ src/main/java/cn/lexed/git/HelloGits.java
@@ -0,0 +1,26 @@
+package cn.lexed.git;
+
+
+/**
+* @ClassName: HelloGits
+* @Description: 
+* @author 大来老师
+* @date 2021年8月25日
+* @version V1.0 
+*/
+public class HelloGits {
+
+	/**
+	* @Title: main
+	* @Description: 方法说明
+	* @param @param args
+	* @return void
+	* @throws
+	*/
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println("git管理");
+         
+	}
+
+}
